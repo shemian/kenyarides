5ccd0d5ef5bc5ae42ecc1f130f4518d4d68e4918
shemian <shemian092@gmail.com>
2022-09-09 15:04:49 -0700

    Updated the index page

---
+++ includes/navbar.php
@@ -0,0 +1,46 @@
+<header class="header-nav menu_style_home_one transparent main-menu">
+    <!-- Ace Responsive Menu -->
+    <nav> 
+      <div class="container posr"> 
+        <!-- Menu Toggle btn-->
+        <div class="menu-toggle">
+          <button type="button" id="menu-btn">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+        </div>
+        <a href="index.html" class="navbar_brand float-start dn-md">
+          <img class="logo1 img-fluid" src="images/header-logo.svg" alt="header-logo.svg">
+          <img class="logo2 img-fluid" src="images/header-logo2.svg" alt="header-logo2.svg">
+				</a>
+        <!-- Responsive Menu Structure-->
+        <ul id="respMenu" class="ace-responsive-menu text-end" data-menu-style="horizontal">
+          <li> <a href="index.php"><span class="title">Home</span></a></li>
+          <li> <a href="#"><span class="title">List A car</span></a></li>
+          <li> <a href="#"><span class="title">Listings</span></a></li>
+          <li> <a href="page-about.html"><span class="title">About Us</span></a></li>
+          <li> <a href="page-faq.html"><span class="title">FAQs</span></a></li>
+          <li> <a href="page-contact.html"><span class="title">Contact Us</span></a></li>
+
+          <li> <a href="#"><span class="title">User Info</span></a>
+              <ul>
+                <li><a href="page-dashboard.html">Dashboard</a></li>
+                <li><a href="page-dashboard-profile.html">Profile</a></li>
+                <li><a href="page-dashboard-listing.html">My Listing</a></li>
+                <li><a href="page-dashboard-favorites.html">Favorites</a></li>
+                <li><a href="page-dashboard-add-listings.html">Add Listing</a></li>
+                <li><a href="page-dashboard-messages.html">Messages</a></li>
+                <li><a href="page-login.html">Logout</a></li>
+              </ul>
+          </li>
+          
+         
+          </li>
+         
+          <li class="add_listing"><a href="page-dashboard-add-listings.html">+ Add Listing</a></li>
+          <li class="sidebar_panel"><a class="sidebar_switch pt0" href="#"><span></span></a></li>
+        </ul>
+      </div>
+    </nav>
+  </header>
