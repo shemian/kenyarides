6099896dd476a9acff63138df326e1b733218b8c
shemian <shemian092@gmail.com>
2022-09-18 03:16:50 -0700

    Deigned the footer

--- includes/footer.php
+++ includes/footer.php
@@ -2,9 +2,7 @@
     <div class="container">
       <div class="row">
         <div class="col-md-4 col-xl-7">
-          <div class="footer_about_widget text-start">
-            <div class="logo mb40 mb0-sm"><img src="images/header-logo.svg" alt="header-logo.png"></div>
-          </div>
+         
         </div>
         <div class="col-md-8 col-xl-5">
           <div class="footer_menu_widget text-start text-md-end mt15">
@@ -26,14 +24,14 @@
         <div class="col-sm-6 col-md-4 col-lg-3 col-xl-3">
           <div class="footer_about_widget">
             <h5 class="title">OFFICE</h5>
-            <p>Germany —<br>329 Queensberry Street,<br>North Melbourne VIC 3051</p>
+            <p>Nairobi  —<br>Kenya</p>
           </div>
         </div>
         <div class="col-sm-6 col-md-4 col-lg-3 col-xl-3">
           <div class="footer_contact_widget">
             <h5 class="title">NEED HELP</h5>
-            <div class="footer_phone">+1 670 936 46 70</div>
-            <p>hello@voiture.com</p>
+            <div class="footer_phone">+254 7450 241 08</div>
+           
           </div>
         </div>
         <div class="col-sm-6 col-md-4 col-lg-3 col-xl-3">
@@ -42,39 +40,9 @@
             <p>Monday – Friday: 09:00AM – 09:00PM<br>Saturday: 09:00AM – 07:00PM<br>Sunday: Closed</p>
           </div>
         </div>
-        <div class="col-sm-6 col-md-6 col-lg-3 col-xl-3">
-          <div class="footer_contact_widget">
-            <h5 class="title">KEEP IN TOUCH</h5>
-            <form class="footer_mailchimp_form">
-              <div class="wrapper">
-                <div class="col-auto">
-                  <input type="email" class="form-control" placeholder="Enter your email...">
-                  <button type="submit">GO</button>
-                </div>
-              </div>
-            </form>
-            <p>Get latest updates and offers.</p>
-          </div>
-        </div>
+        
       </div>
     </div>
     <div class="container">
-      <div class="row">
-        <div class="col-md-6 col-lg-8 col-xl-9">
-          <div class="copyright-widget mt5 text-start mb20-sm">
-            <p>CreativeLayers © 2022. All Rights Reserved.</p>
-          </div>
-        </div>
-        <div class="col-md-6 col-lg-4 col-xl-3">
-          <div class="footer_social_widget text-start text-md-end">
-            <ul class="mb0">
-              <li class="list-inline-item"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-              <li class="list-inline-item"><a href="#"><i class="fab fa-twitter"></i></a></li>
-              <li class="list-inline-item"><a href="#"><i class="fab fa-instagram"></i></a></li>
-              <li class="list-inline-item"><a href="#"><i class="fab fa-linkedin"></i></a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
+ 
   </section>
