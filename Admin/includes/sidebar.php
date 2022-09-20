775b48a122b8610e4465116800a79b609a9281ff
shemian <shemian092@gmail.com>
2022-09-20 01:55:13 -0700

    included manage bookings page

--- Admin/includes/sidebar.php
+++ Admin/includes/sidebar.php
@@ -3,7 +3,7 @@
                 <div class="list-group list-group-flush">
                     <a class="list-group-item list-group-item-action list-group-item-light p-3 " href="dashboard.php">Dashboard</a>
                     <a class="list-group-item list-group-item-action list-group-item-light p-3" href="manage_vehicle.php">Manage Vehicle</a>
-                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Bookings</a>
+                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="bookings.php">Bookings</a>
                     <a class="list-group-item list-group-item-action list-group-item-light p-3" href="reguser.php">Manage Users</a>
                 </div>
             </div>
